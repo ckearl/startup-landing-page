b4b15511fc5560ea202bc7ff64dac5d97affd0e4
Christopher Kearl <c2_kearl@yahoo.com>
2024-09-17 21:30:55 -0600

    tailwind config

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<div style={{ boxSizing: "border-box", margin: 0, border: 0 }}>
-			<App />
-		</div>
+		<App />
 	</React.StrictMode>
 );
 
